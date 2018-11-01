948793440e4e8cdaa99287fffde514b1c91d2267
Shrimant More <smshrimant@gmail.com>
2018-11-02 00:50:00 +0530

    Update ckbalance.php

--- CheckBalnce/ckbalance.php
+++ CheckBalnce/ckbalance.php
@@ -1,18 +1,16 @@
 <?php
-/*
 $con= mysqli_connect("localhost","phpmyadmin","Reshma@92mo","phpmyadmin");
 if(!$con)
 {
-die("Could not connect".mysqli_error());
+	die("Could not connect".mysqli_error());
 }
-*/
 ?>
 
 <!DOCTYPE html>
 	<html>
 		<head>
 			<title>
-				Check Balance Form
+				Check balance
 			</title>
 <style>
 body
@@ -21,7 +19,7 @@ background:"powederblue";
 background-image: url('iot10.jpg');
 }
 .wrap{
-	width:400px;
+	width:600px;
 	margin:auto;
 	bakground-color:#ffff46;
 	background-image: url('white1.jpg');
@@ -47,16 +45,18 @@ border-radius:5px;
 border:"black";
 }
 input[type=text],input[type=email],input[type=number],input[type=password],input[type=tel]{
-width:90%
+width:48%
 }
 input[type=submit]{
-width:95%;
+width:52%;
 background:orange;
 cursor:pointer;
 font-size:18px;
 font-weight:bold;
-color:white
+color:white;
+
 }
+
 input[type=submit]:hover{
 background:yellow;
 }
@@ -64,6 +64,25 @@ select{
 padding:10px;
 width:32%;
 border-radius:5px;
+}
+
+#customers {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+#customers td, #customers th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+#customers th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: orange;
+    color: white;
 }
 </style>
 </head>
@@ -74,27 +93,69 @@ border-radius:5px;
 <br>
 <br>
 <br>
-<br>
-<br>
-<br>
-<br>
 
 	<div class="wrap">
 		
 		<form method="post">
 		
-			<h2>Check Balance</h2>
+			<h2>Check User Balance</h2>
 
 
-<input type="text" name="userid" id="userid" placeholder= " User ID"><br>
+<center>
+<input type="text" name="id" id="id" placeholder="User ID" required>
+<br>
+</center>
 
-<input type="submit" name="signup" id="signup" value=" SUBMIT">
-
+<center>
+<input type="submit" name="Signup" value="SUBMIT">
+</center>
 
 <br>
-<input type="text" name="userid" id="amount" placeholder= " AMOUNT " ><br>
+
+
+
+
+
+<?php
+
+echo "<table align='center' border='1' id='customers'>
+<tr>
+<th> Name</th>
+<th> Balance</th>
+
+</tr>";
+
+if(isset($_POST['Signup']))
+{
+$id=$_POST['id'];
+				
+				$query="SELECT * FROM BusUserData WHERE userid='$id'";
+				$query_run=mysqli_query($con,$query);
+				//$query_run1=mysqli_num_rows($query_run);
+				if($fetch=mysqli_fetch_array($query_run))
+				{
+					echo "<tr>";
+					echo "<td>" . $fetch['name'] . "</td>";
+					echo "<td>" . $fetch['recharge'] . "</td>";
+					
+					echo "</tr>";
+				}
+
+echo "</table>";
+//header("location: retrive.php");
+}
+?>
+
+
+
+
+
+
 
 </form>
+
+
+
 
 </body>
 </html>
